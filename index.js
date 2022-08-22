380d2b11116d14e9166d3aaad3ed783419fb2bde
Brian <bgitau35@gmail.com>
2022-08-22 11:25:39 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (feet > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(checkCity) {
+  if (checkCity === "NYC") {
+    return "Ok, sounds good.";
+  } else if (checkCity != "NYC") {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
